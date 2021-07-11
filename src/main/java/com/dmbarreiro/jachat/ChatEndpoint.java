d6cd4e31eab2cff3b160bd2a923558cc20fa1bcf
dmbarreiro <5133990+dmbarreiro@users.noreply.github.com>
2021-07-12 00:23:18 +0200

    Backup

--- src/main/java/com/dmbarreiro/jachat/ChatEndpoint.java
+++ src/main/java/com/dmbarreiro/jachat/ChatEndpoint.java
@@ -1,20 +1,34 @@
 package com.dmbarreiro.jachat;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
+import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/chat/{username}")
 public class ChatEndpoint {
 
-    @OnOpen
-    public void onOpen(Session session) throws IOException {
+    private Session session;
+    private static Set<ChatEndpoint> chatEndpoints = new CopyOnWriteArraySet<>();
+    private static HashMap<String, String> users = new HashMap<>();
 
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) throws IOException, EncodeException {
+        this.session = session;
+        chatEndpoints.add(this);
+        users.put(session.getId(), username);
+
+        String message = username + "Connected!";
+        broadcast(message);
     }
 
     @OnMessage
@@ -30,5 +44,17 @@ public class ChatEndpoint {
     @OnError
     public void onError(Session session, Throwable throwable) {
 
+    }
+
+    private static void broadcast(String message) throws IOException, EncodeException {
+        chatEndpoints.forEach(endpoint -> {
+            synchronized (endpoint) {
+                try {
+                    endpoint.session.getBasicRemote().sendObject(message);
+                } catch (IOException | EncodeException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
